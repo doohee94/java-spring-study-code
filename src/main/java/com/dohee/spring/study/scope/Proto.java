dab9546e59c957a774bfc51cf5873550833424f8
doohee94 <doohee94@naver.com>
2021-03-30 23:09:18 +0900

    인프런 scope 강의 코드

---
+++ src/main/java/com/dohee/spring/study/scope/Proto.java
@@ -0,0 +1,9 @@
+package com.dohee.spring.study.scope;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class Proto {
+}
---
+++ src/main/java/com/dohee/spring/study/scope/Single.java
@@ -0,0 +1,15 @@
+package com.dohee.spring.study.scope;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Single {
+
+    @Autowired
+    private Proto proto;
+
+    public Proto getProto() {
+        return proto;
+    }
+}
